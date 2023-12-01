befb6c5ab6eec56b61e1d5edc14c9a8d0e8c7599
sabitha-kuppusamy <148762963+sabitha-kuppusamy@users.noreply.github.com>
2023-12-01 13:10:33 +0000

    FIX Web XSS attack

--- Projects/Weather/script.js
+++ Projects/Weather/script.js
@@ -19,19 +19,19 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, op
         console.log(response)
 
         //different property defined 
-        cloud_pct.innerHTML = response.cloud_pct
-        temp.innerHTML = response.temp
-        temp2.innerHTML = response.temp
-        feels_like.innerHTML = response.feels_like
-        humidity.innerHTML = response.humidity
-        humidity2.innerHTML = response.humidity
-        min_temp.innerHTML = response.min_temp
-        max_temp.innerHTML = response.max_temp
-        wind_speed.innerHTML = response.wind_speed
-        wind_speed2.innerHTML = response.wind_speed
+        cloud_pct.innerHTML =  DOMPurify.sanitze(response.cloud_pct)
+        temp.innerHTML = DOMPurify.sanitze( response.temp)
+        temp2.innerHTML = DOMPurify.sanitze( response.temp)
+        feels_like.innerHTML = DOMPurify.sanitze( response.feels_like)
+        humidity.innerHTML =  DOMPurify.sanitze(response.humidity)
+        humidity2.innerHTML = DOMPurify.sanitze( response.humidity)
+        min_temp.innerHTML = DOMPurify.sanitze( response.min_temp)
+        max_temp.innerHTML =  DOMPurify.sanitze(response.max_temp)
+        wind_speed.innerHTML = DOMPurify.sanitze( response.wind_speed)
+        wind_speed2.innerHTML = DOMPurify.sanitze (response.wind_speed)
       //  wind_degrees.innerHTML = response.wind_degrees
-        sunrise.innerHTML = response.sunrise
-        sunset.innerHTML = response.sunset
+        sunrise.innerHTML = DOMPurify.sanitze( response.sunrise)
+        sunset.innerHTML =  DOMPurify.sanitze(response.sunset)
     })
     .catch(err => console.error(err));
 }
